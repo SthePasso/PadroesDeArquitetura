ec4d508a9f11ee0aad2aec0e9b0fee9b897d0d70
SthePasso <sthefaniepasso@gmail.com>
2021-06-18 09:11:51 -0400

    Padrao Decorador Estrutural

---
+++ 3_Padrão_Decorator/HouseBlend.java
@@ -0,0 +1,10 @@
+public class HouseBlend extends Beverage{
+    
+    public HouseBlend(){
+        description = "House Blend Coffe";
+    }
+
+    public double cost(){
+        return .89;
+    }
+}
